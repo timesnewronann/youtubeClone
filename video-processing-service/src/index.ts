d7a0ad1d8117be0697f645f6481d28cb2ea33676
timesnewronann <ronanborja26@gmail.com>
2025-01-22 19:47:10 -0800

    YoutubeClone 1.1.5: Updated index.ts post working on proper solution for async(req,res) overloading issue so can have better safety.

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,21 +1,55 @@
-import express from "express";
-import Ffmpeg from "fluent-ffmpeg";
+import express, {Request, Response} from "express";
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadProcessedVideo } from "./storage";
+
+//call setup directories 
+setupDirectories();
 
 const app = express(); 
 //initialize json for requests
 app.use(express.json());
 
 
-app.post("/process-video", (req,res) => {
-    // Get path of the input video file from the request body 
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
-
-    //error handling technique to check that this isn't undefined and the output file is not undefined
-    if (!inputFilePath || !outputFilePath) {
-        res.status(400).send("Bad Request: Missing file path."); //client gave us wrong parameters
+app.post("/process-video", async(req,res):Promise<any> => {
+    // Get the bucket and filename from the Cloud Pub/Sub message (message queue)
+    let data;
+    try {
+        const message = Buffer.from(req.body.message.data,'base64').toString('utf-8');
+        data = JSON.parse(message);
+        if (!data.name){
+            throw new Error('Invalid message payload received.');
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send('Bad Request: missing filename.');
     }
- 
+
+    const inputFileName = data.name;
+    const outputFileName = `processed-${inputFileName}`;
+    
+    //download the raw video from cloud storage
+    await downloadRawVideo(inputFileName);
+
+     // Convert the video to 360p
+     try {
+        await convertVideo(inputFileName, outputFileName);
+     } catch (err){
+        await Promise.all([
+        deleteRawVideo(inputFileName),
+        deleteProcessedVideo(outputFileName)
+        ]);
+        console.error(err);
+        return res.status(500).send('Internal Server Error: video processing failed.');
+     }
+
+    //Upload the processed video to cloud storage
+    await uploadProcessedVideo(outputFileName);
+
+    await Promise.all([
+        deleteRawVideo(inputFileName),
+        deleteProcessedVideo(outputFileName)
+    ]);
+
+    return res.status(200).send('Processing Finished successfully');
 });
 
 // set port 
--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -4,6 +4,7 @@
 import {Storage} from '@google-cloud/storage';
 import fs from 'fs';
 import Ffmpeg from 'fluent-ffmpeg';
+import { dir } from 'console';
 
 // creating an instance of Google Cloud Storage 
 const storage = new Storage();
@@ -88,7 +89,56 @@ export async function uploadProcessedVideo(fileName:string) {
 }
 
 /**
+ * @param fileName - The name of the file to delete from the
+ * {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ */
+export function deleteRawVideo(fileName:string){
+    return deleteFile(`${localRawVideoPath}/${fileName}`);
+}
+
+/**
+ * @param fileName - The name of the file to delete from the 
+ * {@link localProcessedVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ */
+export function deleteProcessedVideo(fileName:string){
+    return deleteFile(`${localProcessedVideoPath}/${fileName}`);
+}
+
+/**
  * @param filePath - The path of the file to delete.
  * @returns A promise that resolves when the file has been deleted.
  */
+function deleteFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        // check if the files exist
+        if(fs.existsSync(filePath)){
+            //delete the file
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    //delete was unsuccessful
+                    console.log(`Failed to delete file at ${filePath}, err`);
+                    reject(err);
+                } else {
+                    console.log(`File deleted at ${filePath}`);
+                    resolve();
+                }
+            })
+        } else{
+            console.log(`File not found at ${filePath}, skipping the delete.`);
+            resolve()
+        }
+    })
+}
 
+/**
+ * Ensures a director exists, creating it if necessary.
+ * @param {string} dirPath - The directory path to check.
+ */
+function ensureDirectoryExistence(dirPath: string){
+    if(!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, {recursive: true}); // recusrive: true enables creating nesed directories
+        console.log(`Directory created at ${dirPath}`);
+    }
+}
