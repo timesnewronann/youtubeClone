69da51cdc004d9adf42ddc178809a1476016ab7b
timesnewronann <ronanborja26@gmail.com>
2025-02-06 18:47:37 -0800

    YoutubeClone 1.2.6: Updated index.ts for firebase user creation

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -26,7 +26,7 @@ const firebaseConfig = {
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 const auth = getAuth(app);
 
--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,14 +1,38 @@
+'use client';
+
 import Image from "next/image";
-import styles from "./navbar.module.css"
 import Link from "next/link";
 
+import styles from "./navbar.module.css"
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { unsubscribe } from "diagnostics_channel";
+// closure
+
 export default function Navbar() {
+    // Init user state
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChangedHelper((user) =>{
+            setUser(user);
+        });
+
+        return () => unsubscribe(); // returning this function
+    });
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
                     <Image width={90} height={20}
                      src="/youtube-logo.svg" alt="YouTube Logo"/>
             </Link>
+            {
+                //TODO: Add a upload 
+            }
+            <SignIn user={user}/>
         </nav>
     );
 }
