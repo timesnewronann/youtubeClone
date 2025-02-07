ed85518e86f8a3321e3edd23ce396fd56a061f58
timesnewronann <ronanborja26@gmail.com>
2025-02-06 19:43:18 -0800

    firebase file fixed

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -15,14 +15,14 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyA84CvLXBZKKFbT-bs4FYWRorFmtHpln9E",
-    authDomain: "yt-clone-80b84.firebaseapp.com",
-    projectId: "yt-clone-80b84",
-    appId: "1:663031018613:web:c20790a7f556963fd1f43c",
-    measurementId: "G-X27THPKZFF"
-  };
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  };  
+
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
